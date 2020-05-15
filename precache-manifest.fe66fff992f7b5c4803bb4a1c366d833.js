eee44f7fd33aa32a0d917b48bba16c073320ee08
Bernard Iorver <bernard.benion.iorver@hotmail.com>
2020-05-16 00:54:48 +0100

    Updates

---
+++ precache-manifest.fe66fff992f7b5c4803bb4a1c366d833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72226d6cda1ceb0d8afb6010d080100f",
+    "url": "/benion-firebase-crud/index.html"
+  },
+  {
+    "revision": "e25625fbd33ffca91913",
+    "url": "/benion-firebase-crud/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f78ed0e1ee6f3ec2731a",
+    "url": "/benion-firebase-crud/static/js/2.4990473b.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/benion-firebase-crud/static/js/2.4990473b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25625fbd33ffca91913",
+    "url": "/benion-firebase-crud/static/js/main.e7182464.chunk.js"
+  },
+  {
+    "revision": "d15811310af881c1c6b1",
+    "url": "/benion-firebase-crud/static/js/runtime-main.eccdfd32.js"
+  }
+]);
